4495e1c34abf5ca6944670f4aff511c2d1d86b63
Vini <vnsxarts@gmail.com>
2025-09-16 15:05:15 -0300

    Arquivo do banco para Diagrama

---
+++ zennofinancas.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 16/09/2025 às 17:48
+-- Versão do servidor: 9.1.0
+-- Versão do PHP: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `zennofinancas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `adm`
+--
+
+DROP TABLE IF EXISTS `adm`;
+CREATE TABLE IF NOT EXISTS `adm` (
+  `id_adm` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha_hash` varchar(60) NOT NULL,
+  `nivel_acesso` varchar(8) DEFAULT NULL,
+  `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_adm`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `aportes_objetivo`
+--
+
+DROP TABLE IF EXISTS `aportes_objetivo`;
+CREATE TABLE IF NOT EXISTS `aportes_objetivo` (
+  `id_aporte` int NOT NULL AUTO_INCREMENT,
+  `id_objetivo` int NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `descricao_aporte` varchar(25) NOT NULL,
+  `data_aporte` date NOT NULL,
+  PRIMARY KEY (`id_aporte`),
+  KEY `id_objetivo` (`id_objetivo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id_categoria` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `nome` varchar(25) NOT NULL,
+  `tipo` varchar(7) NOT NULL,
+  PRIMARY KEY (`id_categoria`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `gastos`
+--
+
+DROP TABLE IF EXISTS `gastos`;
+CREATE TABLE IF NOT EXISTS `gastos` (
+  `id_gasto` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `id_categoria` int NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `descricao` varchar(25) DEFAULT NULL,
+  `data_gasto` date NOT NULL,
+  `status_pagamento` varchar(8) NOT NULL,
+  PRIMARY KEY (`id_gasto`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_categoria` (`id_categoria`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `objetivos`
+--
+
+DROP TABLE IF EXISTS `objetivos`;
+CREATE TABLE IF NOT EXISTS `objetivos` (
+  `id_objetivo` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `nome` varchar(25) NOT NULL,
+  `valor_desejado` decimal(10,2) NOT NULL,
+  `data_objetivo` date NOT NULL,
+  `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_objetivo`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `receitas`
+--
+
+DROP TABLE IF EXISTS `receitas`;
+CREATE TABLE IF NOT EXISTS `receitas` (
+  `id_receita` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `id_categoria` int NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `descricao` varchar(25) DEFAULT NULL,
+  `data_receita` date NOT NULL,
+  PRIMARY KEY (`id_receita`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_categoria` (`id_categoria`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `numero` varchar(14) NOT NULL,
+  `senha_hash` varchar(60) NOT NULL,
+  `ultimo_login` datetime NOT NULL,
+  `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
